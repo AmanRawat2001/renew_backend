16789bcf499baafd621736e4e8589bae95bf24af
Aman YS <aman.rawat@your-space.in>
2026-02-27 13:33:08 +0530

    enum state for now

--- app/Enums/SitePage.php
+++ app/Enums/SitePage.php
@@ -6,18 +6,12 @@ enum SitePage: string
 {
     case HOME = 'home';
     case EMPOWERING_LIVES = 'empowering_lives';
-    case ABOUT_US = 'about_us';
-    case SERVICES = 'services';
-    case CONTACT = 'contact';
 
     public function label(): string
     {
         return match ($this) {
             self::HOME => 'Home',
             self::EMPOWERING_LIVES => 'Empowering Lives',
-            self::ABOUT_US => 'About Us',
-            self::SERVICES => 'Services',
-            self::CONTACT => 'Contact',
         };
     }
 }
