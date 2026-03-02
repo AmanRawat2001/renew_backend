ff7ddd814de66fe811b8056ab7984d22b7ba0536
Aman YS <aman.rawat@your-space.in>
2026-03-02 14:38:47 +0530

    update the sidebar

--- resources/views/layouts/app/sidebar.blade.php
+++ resources/views/layouts/app/sidebar.blade.php
@@ -8,27 +8,26 @@
 </head>
 
 <body class="min-h-screen bg-white dark:bg-zinc-800">
-    <flux:sidebar sticky collapsible="mobile"
-        class="border-e border-zinc-200 bg-zinc-50 dark:border-zinc-700 dark:bg-zinc-900">
+    <flux:sidebar sticky collapsible class="bg-zinc-50 dark:bg-zinc-900 border-r border-zinc-200 dark:border-zinc-700">
         <flux:sidebar.header>
             <x-app.logo :sidebar="true" href="{{ route('dashboard') }}" wire:navigate />
-            <flux:sidebar.collapse class="lg:hidden" />
+            <flux:sidebar.collapse
+                class="in-data-flux-sidebar-on-desktop:not-in-data-flux-sidebar-collapsed-desktop:-mr-2" />
         </flux:sidebar.header>
 
         <flux:sidebar.nav>
             <flux:sidebar.group :heading="__('Pages')" class="grid">
-                <flux:sidebar.item icon="home" :href="route('admin.home')"
-                    :current="request()->routeIs('admin.home')" wire:navigate>
+                <flux:sidebar.item icon="home" :href="route('admin.home')" :current="request()->routeIs('admin.home')"
+                    wire:navigate>
                     {{ __('Home Page') }}
                 </flux:sidebar.item>
-                <flux:sidebar.item icon="home" :href="route('admin.program')"
-                    :current="request()->routeIs('program.*')" wire:navigate>
+                <flux:sidebar.item icon="bolt" :href="route('admin.program')"
+                    :current="request()->routeIs('admin.program')" wire:navigate>
                     {{ __('Empowering Lives') }}
                 </flux:sidebar.item>
             </flux:sidebar.group>
 
-            <flux:sidebar.group :heading="__('Home Page Sections')" collapsible>
-
+            <flux:sidebar.group :heading="__('Home Page Sections')" expandable>
                 <flux:sidebar.item icon="photo" :href="route('admin.sliders.index')"
                     :current="request()->routeIs('admin.sliders.*')" wire:navigate>
                     {{ __('Slider Section') }}
@@ -53,10 +52,9 @@
                     :current="request()->routeIs('admin.mission_sliders.*')" wire:navigate>
                     {{ __('Mission Sliders') }}
                 </flux:sidebar.item>
-
             </flux:sidebar.group>
-            <flux:sidebar.group :heading="__(' Program Page ')" collapsible>
 
+            <flux:sidebar.group :heading="__('Program Page')" expandable>
                 <flux:sidebar.item icon="photo" :href="route('admin.main_sliders.index')"
                     :current="request()->routeIs('admin.main_sliders.*')" wire:navigate>
                     {{ __('Slider Sections') }}
@@ -66,25 +64,59 @@
                     :current="request()->routeIs('admin.other_sections.*')" wire:navigate>
                     {{ __('Details Section') }}
                 </flux:sidebar.item>
+
                 <flux:sidebar.item icon="squares-2x2" :href="route('admin.other_feature_cards.index')"
                     :current="request()->routeIs('admin.other_feature_cards.*')" wire:navigate>
                     {{ __('Feature Cards') }}
                 </flux:sidebar.item>
+
                 <flux:sidebar.item icon="chart-bar" :href="route('admin.other_impacts.index')"
                     :current="request()->routeIs('admin.other_impacts.*')" wire:navigate>
                     {{ __('Impact Section') }}
                 </flux:sidebar.item>
+
                 <flux:sidebar.item icon="rocket-launch" :href="route('admin.other_mission_sliders.index')"
                     :current="request()->routeIs('admin.other_mission_sliders.*')" wire:navigate>
                     {{ __('Mission Sliders') }}
                 </flux:sidebar.item>
-
             </flux:sidebar.group>
         </flux:sidebar.nav>
 
-        <flux:spacer />
+        <flux:sidebar.spacer />
 
-        <x-app.user-menu class="hidden lg:block" :name="auth()->user()->name" />
+        <flux:sidebar.nav>
+            <flux:sidebar.item icon="cog-6-tooth" :href="route('profile.edit')" wire:navigate>
+                {{ __('Settings') }}
+            </flux:sidebar.item>
+        </flux:sidebar.nav>
+
+        <flux:dropdown position="top" align="start" class="max-lg:hidden">
+            <flux:sidebar.profile :initials="auth()->user()->initials()" name="{{ auth()->user()->name }}" />
+
+            <flux:menu>
+                <flux:menu.radio.group>
+                    <div class="p-0 text-sm font-normal">
+                        <div class="flex items-center gap-2 px-1 py-1.5 text-start text-sm">
+                            <flux:avatar :name="auth()->user()->name" :initials="auth()->user()->initials()" />
+                            <div class="grid flex-1 text-start text-sm leading-tight">
+                                <flux:heading class="truncate">{{ auth()->user()->name }}</flux:heading>
+                                <flux:text class="truncate">{{ auth()->user()->email }}</flux:text>
+                            </div>
+                        </div>
+                    </div>
+                </flux:menu.radio.group>
+
+                <flux:menu.separator />
+
+                <form method="POST" action="{{ route('logout') }}" class="w-full">
+                    @csrf
+                    <flux:menu.item as="button" type="submit" icon="arrow-right-start-on-rectangle"
+                        class="w-full cursor-pointer" data-test="logout-button">
+                        {{ __('Log Out') }}
+                    </flux:menu.item>
+                </form>
+            </flux:menu>
+        </flux:dropdown>
     </flux:sidebar>
 
 
