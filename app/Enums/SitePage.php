52dbed32afef47c754ba523dc3217ff825b57f65
Aman YS <aman.rawat@your-space.in>
2026-03-09 10:31:35 +0530

    update the enum for the sitepage

--- app/Enums/SitePage.php
+++ app/Enums/SitePage.php
@@ -6,12 +6,14 @@ enum SitePage: string
 {
     case HOME = 'home';
     case EMPOWERING_LIVES = 'empowering_lives';
+    case ACCELERATING_INNOVATION = 'accelerating_innovation';
 
     public function label(): string
     {
         return match ($this) {
             self::HOME => 'Home',
             self::EMPOWERING_LIVES => 'Empowering Lives',
+            self::ACCELERATING_INNOVATION => 'Accelerating Innovation',
         };
     }
 }
